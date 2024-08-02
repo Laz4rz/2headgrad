9fdb3178c0badf3d6722707e5840459e36749f16
Laz4rz <62252332+Laz4rz@users.noreply.github.com>
2024-08-02 21:58:13 +0200

    backprop: works correctly up to manual tanh

--- head2grad.py
+++ head2grad.py
@@ -15,6 +15,7 @@ class Value:
         return f"Value(data={self.data})"
 
     def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self, other), '+')
 
         def _backward():
@@ -24,17 +25,26 @@ class Value:
 
         return out
 
+    # def __sub__(self, other):
+    #     other = other if isinstance(Value) else Value(other)
+    #     out = Value(self.data - other.data, (self, other), '-')
+
+    #     def _backward():
+    #         self.grad += -1.0 * out.grad
+    #         other.grad += -1.0 * out.grad
+    #     out._backward = _backward
+
+    #     return out
+
+    # sub can also be wrote using previously defined operations
     def __sub__(self, other):
-        out = Value(self.data - other.data, (self, other), '-')
-
-        def _backward():
-            self.grad += -1.0 * out.grad
-            other.grad += -1.0 * out.grad
-        out._backward = _backward
-
-        return out
+        return self + (-other)
+    
+    def __neg__(self):
+        return self * (-1)
 
     def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self, other), '*')
 
         def _backward():
@@ -44,13 +54,45 @@ class Value:
 
         return out
     
-    def tanh(self):
-        x = self.data
-        tanh = (math.exp(2*x) - 1) / (math.exp(2*x) + 1)
-        out = Value(tanh, [self], "tanh")
+    def __truediv__(self, other):
+        return self * other ** (-1)
+    
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "only int/float"
+        out = Value(self.data ** other, (self,), f"pow(2)")
 
         def _backward():
-            self.grad = (1  - tanh**2) * out.grad
+            self.grad += other * self.data ** (other - 1) * out.grad
+        out._backward = _backward
+
+        return out
+
+    def __radd__(self, other):
+        return self + other 
+    
+    def __rsub__(self, other):
+        return self - other 
+    
+    def __rmul__(self, other):
+        return self * other 
+    
+    def tanh(self):
+        x = self.data
+        val = (math.exp(2*x) - 1) / (math.exp(2*x) + 1)
+        out = Value(val, [self], "tanh")
+
+        def _backward():
+            self.grad = (1  - val**2) * out.grad # 1 - tanh(x)^2
+        out._backward = _backward
+
+        return out
+    
+    def exp(self):
+        val = math.exp(self.data)
+        out = Value(val, [self], "exp")
+
+        def _backward():
+            self.grad += val * out.grad # e^x
         out._backward = _backward
 
         return out
